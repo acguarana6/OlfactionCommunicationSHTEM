09685c18109df02925efa95bf4def2c7f49bd18c
Srinivasan R <sri@Srinivasans-MacBook-Pro.local>
2020-07-24 13:47:27 -0700

    code for synthesis device to get final int

---
+++ PullDataFromFirebaseTest/PullDataFromFirebaseTest.ino
@@ -0,0 +1,45 @@
+#include <ESP8266WiFi.h> 
+#include <FirebaseArduino.h>
+
+#define FIREBASE_HOST "testerdatabase-dd943.firebaseio.com"
+#define FIREBASE_AUTH "P2HeSuA0wvHC9cqBMBNdQEwFi3vd92kFRGHSGmhT"
+
+#define WIFI_SSID "insertwifissid"
+#define WIFI_PASSWORD "insertwifipassword"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(500);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  
+  Serial.print("Connecting to "); Serial.print(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("Connected to "); Serial.println(WIFI_SSID);
+  Serial.print("IP Address is : "); Serial.println(WiFi.localIP());
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  int x = Firebase.getInt("/finalint"); //need to replace this with getting JsonVariant type here.
+
+  if (isnan(x)) //could also use success() method
+  {
+    Serial.println("Failed to get final smell int.");
+    return;
+  }
+  else
+  {
+    Serial.print("The final smell is smell "); Serial.println(x);
+  }
+
+  delay(100);
+}
